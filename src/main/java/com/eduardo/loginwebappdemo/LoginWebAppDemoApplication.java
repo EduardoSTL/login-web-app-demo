492ada1a8c05879a9219e8bdf8f5a5e4fb69e9d7
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-10 16:45:07 -0600

    Initial commit

---
+++ src/main/java/com/eduardo/loginwebappdemo/LoginWebAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.loginwebappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWebAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginWebAppDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eduardo/loginwebappdemo/LoginWebAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.loginwebappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWebAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
