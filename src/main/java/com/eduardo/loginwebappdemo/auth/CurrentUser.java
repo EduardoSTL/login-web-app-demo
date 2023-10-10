b78d393ace94cb8284803b202c101a0a57690302
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-10 17:31:57 -0600

    added: auth -> CurrentUser.java

---
+++ src/main/java/com/eduardo/loginwebappdemo/auth/CurrentUser.java
@@ -0,0 +1,12 @@
+package com.eduardo.loginwebappdemo.auth;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
