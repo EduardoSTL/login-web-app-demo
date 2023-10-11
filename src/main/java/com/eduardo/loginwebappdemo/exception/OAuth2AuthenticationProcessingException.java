10a707047c5edc8d120a5dd136ddfe23f788520d
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-11 10:48:07 -0600

    updt: exceptions ->

--- src/main/java/com/eduardo/loginwebappdemo/exception/OAuth2AuthenticationProcessingException.java
+++ src/main/java/com/eduardo/loginwebappdemo/exception/OAuth2AuthenticationProcessingException.java
@@ -3,6 +3,8 @@ package com.eduardo.loginwebappdemo.exception;
 import org.springframework.security.core.AuthenticationException;
 
 public class OAuth2AuthenticationProcessingException extends AuthenticationException {
+
+    //validar procesamiento de autenticacion OAuth2
     public OAuth2AuthenticationProcessingException(String msg, Throwable t) {
         super(msg, t);
     }
