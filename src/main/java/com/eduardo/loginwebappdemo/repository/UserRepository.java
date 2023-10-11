8a1cc64f4d7a4e49e58dd5b95e65ac7f105b3fb2
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-11 10:28:04 -0600

    added: repo -> UserRepository.java

---
+++ src/main/java/com/eduardo/loginwebappdemo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.eduardo.loginwebappdemo.repository;
+
+import com.eduardo.loginwebappdemo.model.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
