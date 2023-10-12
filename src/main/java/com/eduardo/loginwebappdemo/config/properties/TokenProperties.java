2bc5b032f49f99b7d3541856784ab71fa3443cb2
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-12 16:58:23 -0600

    added: tk properties

---
+++ src/main/java/com/eduardo/loginwebappdemo/config/properties/TokenProperties.java
@@ -0,0 +1,17 @@
+package com.eduardo.loginwebappdemo.config.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "token")
+@Getter
+@Setter
+public class TokenProperties {
+    private Integer expiresHours;
+    private String domain;
+
+
+}
