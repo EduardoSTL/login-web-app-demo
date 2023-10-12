d224ea7083cb3cd3ff5e87612f867b75cff0053d
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-12 17:11:08 -0600

    added: all properties class

---
+++ src/main/java/com/eduardo/loginwebappdemo/config/properties/CorsProperties.java
@@ -0,0 +1,16 @@
+package com.eduardo.loginwebappdemo.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+    private String[] allowedOrigins;
+
+    public String[] getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(String[] allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+}
---
+++ src/main/java/com/eduardo/loginwebappdemo/config/properties/OauthProperties.java
@@ -0,0 +1,17 @@
+package com.eduardo.loginwebappdemo.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "oauth")
+public class OauthProperties {
+
+    private String[] authorizedRedirectUris;
+
+    public String[] getAuthorizedRedirectUris() {
+        return authorizedRedirectUris;
+    }
+
+    public void setAuthorizedRedirectUris(String[] authorizedRedirectUris) {
+        this.authorizedRedirectUris = authorizedRedirectUris;
+    }
+}
