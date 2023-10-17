1ba6cc8e5f46edd52da3348dc29d37c5cd5b6779
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-17 14:28:52 -0600

    added: AuthController

---
+++ src/main/java/com/eduardo/loginwebappdemo/controller/AuthController.java
@@ -0,0 +1,85 @@
+package com.eduardo.loginwebappdemo.controller;
+
+import com.eduardo.loginwebappdemo.config.security.TokenProvider;
+import com.eduardo.loginwebappdemo.enums.Authority;
+import com.eduardo.loginwebappdemo.exception.BadRequestException;
+import com.eduardo.loginwebappdemo.model.AuthProvider;
+import com.eduardo.loginwebappdemo.model.entities.User;
+import com.eduardo.loginwebappdemo.payload.ApiResponse;
+import com.eduardo.loginwebappdemo.payload.AuthResponse;
+import com.eduardo.loginwebappdemo.payload.LoginRequest;
+import com.eduardo.loginwebappdemo.payload.SignUpRequest;
+import com.eduardo.loginwebappdemo.repository.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = tokenProvider.createToken(authentication);
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            throw new BadRequestException("Email address already in use.");
+        }
+
+        // Creating user's account
+        User user = new User();
+        user.setName(signUpRequest.getName());
+        user.setEmail(signUpRequest.getEmail());
+        user.setPassword(signUpRequest.getPassword());
+        user.setAuthorities(Set.of(Authority.USER));
+        user.setProvider(AuthProvider.LOCAL);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        User result = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/user/me")
+                .buildAndExpand(result.getId()).toUri();
+
+        return ResponseEntity.created(location)
+                .body(new ApiResponse(true, "User registered successfully@"));
+    }
+}
