e2dda251f52c88c15c49e4ceec0ccbda89c2ee34
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-10-16 20:46:15 -0600

    added: security files

---
+++ src/main/java/com/eduardo/loginwebappdemo/config/security/TokenAuthenticationFilter.java
@@ -0,0 +1,55 @@
+package com.eduardo.loginwebappdemo.config.security;
+
+import com.eduardo.loginwebappdemo.model.entities.User;
+import com.eduardo.loginwebappdemo.service.UsuarioService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @Autowired
+    private UsuarioService customUserService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String jwt = getJwtFromRequest(request);
+
+            if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+                Integer userId = tokenProvider.getUserIdFromToken(jwt);
+
+                User userDetails = customUserDetailsService.loadUserById(userId);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception ex) {
+            logger.error("Could not set user authentication in security context", ex);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/eduardo/loginwebappdemo/config/security/TokenProvider.java
@@ -0,0 +1,55 @@
+package com.eduardo.loginwebappdemo.config.security;
+
+import com.eduardo.loginwebappdemo.config.properties.TokenProperties;
+import com.eduardo.loginwebappdemo.model.entities.User;
+import com.eduardo.loginwebappdemo.util.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Service
+@RequiredArgsConstructor
+public class TokenProvider {
+
+    private final JwtEncoder jwtEncoder;
+    private final JwtDecoder jwtDecoder;
+
+    private final TokenProperties tokenProperties;
+
+    public String createToken(Authentication authentication) {
+        User userPrincipal = (User) authentication.getPrincipal();
+        Instant now = Instant.now();
+
+        var jwtClaimsSet = JwtClaimsSet.builder()
+                .issuer("API Meu Egresso - UFPA")
+                .subject(userPrincipal.getUsername())
+                .issuedAt(now)
+                .expiresAt(now.plus(tokenProperties.getExpiresHours(), ChronoUnit.HOURS))
+                .claim(JwtUtils.NOME.getPropriedade(), userPrincipal.getName())
+                .claim(JwtUtils.USER_ID.getPropriedade(), userPrincipal.getId())
+                .claim(JwtUtils.SCOPE.getPropriedade(), authentication.getAuthorities());
+        return jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet.build())).getTokenValue();
+    }
+
+    public Integer getUserIdFromToken(String token) {
+        Jwt jwt = jwtDecoder.decode(token);
+        return Integer.parseInt(jwt.getClaim(JwtUtils.USER_ID.getPropriedade()).toString());
+    }
+
+    public boolean validateToken(String authToken) {
+        return (!isTokenExpired(authToken));
+    }
+
+    private boolean isTokenExpired(String token) {
+        Instant exp = jwtDecoder.decode(token).getExpiresAt();
+        if (exp != null) {
+            return exp.isBefore(Instant.now());
+        } else {
+            return true;
+        }
+    }
+}
